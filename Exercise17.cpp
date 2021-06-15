8af88ec79b1e212691539993b7777e41710c91ac
hicongtuoc <hicongtuoc@gmail.com>
2021-06-15 16:32:02 +0700

    Update Exercise17.cpp

--- Exercise17.cpp
+++ Exercise17.cpp
@@ -1,54 +1,71 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n,a[100],ok=1;
+int n, a[100], ok = 1;
 
-void init(void){
-    cin>>n;
-    for(int i=n;i>0;i--)   a[i]=i;
+void init(void)
+{
+    cin >> n;
+    for (int i = n; i > 0; i--)
+        a[n - i + 1] = i;
 }
 
-void Swap(int &a, int &b){
-    int t=a;a=b;b=t;
+void Swap(int &a, int &b)
+{
+    int t = a;
+    a = b;
+    b = t;
 }
 
-void hoanVi(void){
-    int j=n-1;
-    while(j>0 && a[j]<a[j+1]) j--;
-    if(j>=0){
-        int k=1;
-        while(a[j]<a[k]) k--;
-        Swap(a[j],a[k]);
-        int l=j+1,r=n;
-        while(l>r){
-            Swap(a[l],a[r]);
-            r++;
-            l--;
+void hoanVi(void)
+{
+    int j = n - 1;
+    while (j > 0 && a[j] < a[j + 1])
+        j--;
+    if (j > 0)
+    {
+        int k = n;
+        while (a[j] < a[k])
+            k--;
+        Swap(a[j], a[k]);
+        int l = j + 1, r = n;
+        while (l < r)
+        {
+            Swap(a[l], a[r]);
+            l++;
+            r--;
         }
     }
-    else ok=0;
+    else
+        ok = 0;
 }
 
-void output(){
-    for(int i=1;i<=n;i++) cout<<a[i];
-    cout<<" ";
+void output()
+{
+    for (int i = 1; i <= n; i++)
+        cout << a[i];
+    cout << " ";
 }
 
-void run(){
-    ok=1;
-    while(ok){
+void run()
+{
+    ok = 1;
+    while (ok)
+    {
         output();
         hoanVi();
     }
-    cout<<endl;
+    cout << endl;
 }
 
-int main(){
-	int test;
-    cin>>test;
-    while(test--){
+int main()
+{
+    int test;
+    cin >> test;
+    while (test--)
+    {
         init();
         run();
     }
 
-	return 0;
+    return 0;
 }
